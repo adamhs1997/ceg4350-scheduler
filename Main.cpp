44e7c8df1c57da86de18c346882ad5baa34adce8
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-06-26 21:53:11 -0400

    Add Process test code

--- Main.cpp
+++ Main.cpp
@@ -8,14 +8,29 @@ Project 1
 //Driver program for the scheduler simulation
 
 #include "FileHandler.h"
+#include "Process.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+	//Read data from the input file
+	//TODO: Change this from the hard-set processes file to the cmd arg
 	FileHandler mfh;
 	int* arr = mfh.parseData("processes.txt");
         cout << "# processes " << mfh.getNumberProcesses() << "\n";
 	for (int i = 0; i < mfh.getNumberProcesses() * 3; i++) {
 		cout << arr[i] << " ";
 	}
-	
+
+	//PROCESSES WILL BE CREATED IN EACH SCHEDULER ALGO
+	//TODO: Make switch stmt that goes by whatever algo passed in
+
+
+	//Test code for process class...
+	cout << "\n";
+	Process testProcess(1, 3);
+	cout << testProcess.getTimeRemaining() << "\n";
+	cout << testProcess.getBurstTime() << "\n";
+	cout << testProcess.hasArrived() << "\n";
+	cout << testProcess.getPid() << "\n";
+
 	return 0;
 }
