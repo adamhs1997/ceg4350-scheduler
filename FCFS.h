adfdf4caf5f653461744b7b8ed2e6295482322df
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-06-26 22:18:09 -0400

    Add plan for FCFS

---
+++ FCFS.h
@@ -0,0 +1,14 @@
+/*
+FCFS algo:
+Will need to take in the process array as param
+Begin some "clock" to monitor arrival times
+When each process arrives, create it and place in queue
+When current running process finishes, mark terminated
+Pick up and finish next process
+**RUN INFINITELY, LET USER KILL SCHEDULER (jic)**
+*/
+class FCFS {
+public:
+	
+};
+
