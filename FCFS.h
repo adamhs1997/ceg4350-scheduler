cce9a6c13661a651902ce66faab3c4324b05a51e
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-06-28 14:43:09 -0400

    Assemble structure of FCFS class

--- FCFS.h
+++ FCFS.h
@@ -7,8 +7,30 @@ When current running process finishes, mark terminated
 Pick up and finish next process
 **RUN INFINITELY, LET USER KILL SCHEDULER (jic)**
 */
+
+#include "Process.h"
+#include <Queue>
+
+using namespace std;
+
 class FCFS {
+private:
+	queue<Process> m_readyQueue;
+	int* m_processInfo;
+	void schedule();
+
 public:
-	
+	FCFS(int* processInfo); // Array is to make all necc processes
+		
 };
+
+//Build the FCFS scheduler object
+FCFS::FCFS(int* processInfo) {
+	m_processInfo = processInfo;
+}
+
+//The FCFS master scheduler!!!
+void FCFS::schedule() {
+
+}
 
