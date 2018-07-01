351d5133461dd252f9920f816ab8a6a73ba836be
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-07-01 15:35:45 -0400

    Add plan for MLFQ algo

--- MLFQ.h
+++ MLFQ.h
@@ -1,1 +1,35 @@
-#pragma once
+/*
+This will simply utilize existing RR and FCFS algos
+During each clock cycle, must check if sthg in higher level queue
+If pre-empted, priority remains same-->go to end of queue though
+*/
+
+#include "Process.h"
+#include <queue>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+class MLFQ {
+
+private:
+	//Need three ready queues, one for each priority
+	queue<Process> m_p0Queue, m_p1Queue, m_p2Queue;
+	int* m_processArray;
+	int m_numProcesses;
+
+public:
+	MLFQ(int* processInfo, int numProcesses);
+	void schedule();
+
+};
+
+MLFQ::MLFQ(int* processInfo, int numProcesses) {
+	m_processArray = processInfo;
+	m_numProcesses = numProcesses;
+}
+
+void MLFQ::schedule() {
+}
