a066a8223d1b477538de86a0f37ed1a0aa48fe8e
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-07-04 13:36:30 -0400

    Update driver to test MLFQ

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,15 @@ Dr. Yong Pei
 Project 1
 */
 
+#include "stdafx.h"
+
 //Driver program for the scheduler simulation
 
 #include "FileHandler.h"
 #include "Process.h"
 #include "FCFS.h"
 #include "RR.h"
+#include "MLFQ.h"
 
 int main(int argc, char** argv) {
 	//Read data from the input file
@@ -27,12 +30,19 @@ int main(int argc, char** argv) {
 	//TODO: Make switch stmt that goes by whatever algo passed in
 
 	//Testing the FCFS algo
+	cout << "\nFCFS:\n";
 	FCFS fcfs_scheduler(arr, numberProcesses);
 	fcfs_scheduler.schedule();
 
 	//Test RR algo
+	cout << "RR:\n";
 	RR rr_scheduler(arr, numberProcesses, 8);
 	rr_scheduler.schedule();
+
+	//Test MLFQ
+	cout << "MLFQ:\n";
+	MLFQ mlfq_scheduler(arr, numberProcesses);
+	mlfq_scheduler.schedule();
 
 	//Test code for process class...
 	cout << "\n";
