d4c0f8c26143981868b13d484323133eace50c5a
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-07-11 21:17:11 -0400

    Sort out of order processes

--- FileHandler.h
+++ FileHandler.h
@@ -11,6 +11,8 @@ class FileHandler {
 private:
 	int* processArray; //to store the final process info array
 	vector<int> accum; //vector for easy internal handling
+	vector<int> timeSort(vector<int> processes);
+
 public:
 	int* parseData(string filename);
 	int getNumberProcesses();
@@ -34,6 +36,9 @@ int* FileHandler::parseData(string filename) {
 	//Pop off erroneous last digit in vector
 	accum.pop_back();
 
+	//Sort the processes by arrival time
+	accum = timeSort(accum);
+
 	//Convert vector to array
 	processArray = &accum[0];
 
@@ -43,5 +48,34 @@ int* FileHandler::parseData(string filename) {
 //Get the number of processes to be scheduled
 int FileHandler::getNumberProcesses() {
 	return accum.size() / 3;
+}
+
+//Bubble sorts the process array
+vector<int> FileHandler::timeSort(vector<int> processes) {
+	vector<int> sortedProcesses;
+	while (!processes.empty()) {
+		//Find smallest arrive time
+		int currentSmallest = 999999;
+		int currentSmallestIndex = -1;
+		for (size_t i = 1; i < processes.size(); i += 3) {
+			if (processes.at(i) < currentSmallest) {
+				currentSmallest = processes.at(i);
+				currentSmallestIndex = i;
+			}
+		}
+
+		//Store the sorted processes
+		sortedProcesses.push_back(processes.at(currentSmallestIndex - 1));
+		sortedProcesses.push_back(processes.at(currentSmallestIndex));
+		sortedProcesses.push_back(processes.at(currentSmallestIndex + 1));
+
+		//Get rid of the stored processes
+		for (size_t i = 0; i < 3; i++)
+			processes.erase(processes.begin() + (currentSmallestIndex - 1));
+		
+	}
+
+	return sortedProcesses;
+
 }
 
